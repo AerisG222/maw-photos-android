4d93e01bf476a9bf4a8ae6b2ca4b9d90a19ab613
Mike Morano <mmorano@mikeandwan.us>
2025-08-08 17:58:48 -0400

    correct upload path

--- MaWPhotos/src/main/kotlin/us/mikeandwan/photos/api/UploadApi.kt
+++ MaWPhotos/src/main/kotlin/us/mikeandwan/photos/api/UploadApi.kt
@@ -6,6 +6,6 @@ import retrofit2.http.*
 
 internal interface UploadApi {
     @Multipart
-    @POST("upload/upload")
+    @POST("upload/")
     suspend fun uploadFile(@Part file: MultipartBody.Part): Response<FileOperationResult>
 }
