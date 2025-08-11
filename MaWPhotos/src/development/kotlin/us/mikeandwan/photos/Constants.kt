fb69050321573e995fe309a2d1711b5ca8c60ef9
Mike Morano <mmorano@mikeandwan.us>
2025-08-11 14:46:59 -0400

    cleanup constants

--- MaWPhotos/src/development/kotlin/us/mikeandwan/photos/Constants.kt
+++ MaWPhotos/src/development/kotlin/us/mikeandwan/photos/Constants.kt
@@ -2,6 +2,4 @@ package us.mikeandwan.photos
 
 object Constants {
     const val API_BASE_URL = "https://dev-media.mikeandwan.us:8081"
-
-    const val WWW_BASE_URL = "https://dev.www.mikeandwan.us:5021"
 }
--- MaWPhotos/src/production/kotlin/us/mikeandwan/photos/Constants.kt
+++ MaWPhotos/src/production/kotlin/us/mikeandwan/photos/Constants.kt
@@ -1,7 +1,5 @@
 package us.mikeandwan.photos
 
 object Constants {
-    const val API_BASE_URL = "https://api.mikeandwan.us"
-    const val AUTH_BASE_URL = "https://auth.mikeandwan.us"
-    const val WWW_BASE_URL = "https://www.mikeandwan.us"
+    const val API_BASE_URL = "https://media.mikeandwan.us"
 }
