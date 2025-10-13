436cabdabf83e749774a1726b8d54d1090c5e5d8
Mike Morano <mmorano@mikeandwan.us>
2025-10-13 06:30:08 -0400

    update dev port

--- MaWPhotos/src/development/kotlin/us/mikeandwan/photos/Constants.kt
+++ MaWPhotos/src/development/kotlin/us/mikeandwan/photos/Constants.kt
@@ -1,5 +1,5 @@
 package us.mikeandwan.photos
 
 object Constants {
-    const val API_BASE_URL = "https://dev-media.mikeandwan.us:8081"
+    const val API_BASE_URL = "https://dev-media.mikeandwan.us:8091"
 }
