632dd4e83d4299f4a2504fc9280d67bdb76c5071
Mike Morano <mmorano@mikeandwan.us>
2026-01-31 10:52:08 -0500

    initial support for spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,41 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.androidx.baselineprofile) apply false
     alias(libs.plugins.androidx.room) apply false
+    alias(libs.plugins.spotless) apply false
+}
+
+subprojects {
+    apply(plugin = "com.diffplug.spotless")
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("${layout.buildDirectory}/**/*.kt")
+            ktlint()
+            trimTrailingWhitespace()
+            leadingTabsToSpaces(4)
+            endWithNewline()
+        }
+
+        kotlinGradle {
+            target("**/*.gradle.kts")
+            targetExclude("${layout.buildDirectory}/**/*.kt")
+            ktlint()
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+
+        format("xml") {
+            target("**/*.xml")
+            targetExclude("**/build/**")
+            trimTrailingWhitespace()
+            leadingTabsToSpaces(4)
+            endWithNewline()
+        }
+
+        format("misc") {
+            target("**/*.md", "**/.gitignore")
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+    }
 }
