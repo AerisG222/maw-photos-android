7da3a7e7927d923fc4a1ad4c6ad8e70af25332a2
Mike Morano <mmorano@mikeandwan.us>
2026-03-04 17:52:13 -0500

    fetch an image when adding the widget to the homescreen

--- MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/widgets/RandomPhotoWidgetReceiver.kt
+++ MaWPhotos/src/main/kotlin/us/mikeandwan/photos/ui/widgets/RandomPhotoWidgetReceiver.kt
@@ -1,7 +1,30 @@
 package us.mikeandwan.photos.ui.widgets
 
+import android.appwidget.AppWidgetManager
+import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import us.mikeandwan.photos.workers.RandomPhotoWorker
 
 class RandomPhotoWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget = RandomPhotoWidget()
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray,
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+
+        // Trigger an immediate fetch when widgets are added or updated
+        val workRequest = OneTimeWorkRequestBuilder<RandomPhotoWorker>().build()
+
+        WorkManager.getInstance(context).enqueueUniqueWork(
+            "RefreshRandomPhotoWidgetOnUpdate",
+            ExistingWorkPolicy.REPLACE,
+            workRequest,
+        )
+    }
 }
