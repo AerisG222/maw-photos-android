75473e3cea53df48f49d338992fcd96470019dd4
Mike Morano <mmorano@mikeandwan.us>
2026-03-08 11:49:53 -0400

    prep release

--- MaWPhotos/build.gradle.kts
+++ MaWPhotos/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "us.mikeandwan.pictures"
         minSdk = 26
         targetSdk = 36
-        versionCode = 82
+        versionCode = 83
         versionName = "9.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
