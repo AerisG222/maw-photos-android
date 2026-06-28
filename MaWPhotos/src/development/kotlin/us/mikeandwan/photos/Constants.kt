13ca7c62c58b8a9177a64542dd2006c8e5b037ee
Mike Morano <mmorano@mikeandwan.us>
2026-06-28 12:27:08 -0400

    allow local network even in prod as it breaks when on home wifi otherwise

--- MaWPhotos/src/development/kotlin/us/mikeandwan/photos/Constants.kt
+++ MaWPhotos/src/development/kotlin/us/mikeandwan/photos/Constants.kt
@@ -2,5 +2,4 @@ package us.mikeandwan.photos
 
 object Constants {
     const val API_BASE_URL = "https://dev-media.mikeandwan.us:8091"
-    const val REQUEST_LOCAL_NETWORK_PERMISSION = true
 }
--- MaWPhotos/src/production/kotlin/us/mikeandwan/photos/Constants.kt
+++ MaWPhotos/src/production/kotlin/us/mikeandwan/photos/Constants.kt
@@ -2,5 +2,4 @@ package us.mikeandwan.photos
 
 object Constants {
     const val API_BASE_URL = "https://media.mikeandwan.us"
-    const val REQUEST_LOCAL_NETWORK_PERMISSION = false
 }
